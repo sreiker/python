4811d229dc4cacf608c96ec377983570ccfd69f5
prashantsharma2608 <132665908+prashantsharma2608@users.noreply.github.com>
2023-10-05 19:46:35 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -7,19 +7,16 @@ import pytest
 # nOt0r5Le5LhYZjrmZ6DpW4QvvXBXTCyG670NB1H02zr1ik9Zwh
 username = "prashantsharma"
 accessToken = "RlEUtZdSXJkl3iEtXNx6eWFSyLBfDJlkYRYG1igfb1OjpXfXRp"
-# build = os.getenv('LT_BUILD_NAME')
+build = os.getenv('LT_BUILD_NAME')
 gridUrl = "hub.lambdatest.com/wd/hub"
 options = ChromeOptions()
 options.browser_version = "112.0"
 options.platform_name = "Windows 11"
 lt_options = {}
-lt_options["username"] = "prashantsharma"
-lt_options["accessKey"] = "nOt0r5Le5LhYZjrmZ6DpW4QvvXBXTCyG670NB1H02zr1ik9Zwh"
 lt_options["project"] = "selenium test"
 lt_options["name"] = "selenium"
-lt_options["build"] = "Hard coded"
+lt_options["build"] = build
 lt_options["network"] = True
-lt_options["networkThrottling"] = "Regular 3G"
 lt_options["w3c"] = True
 lt_options["plugin"] = "python-python"
 # lt_options["tunnel"] = True
